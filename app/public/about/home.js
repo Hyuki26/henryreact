1e889d090014074dfc6849312ba310982cb89284
Yuuri Harayama <hyuki@icstars.org>
2021-03-26 21:20:01 -0500

    hi

--- app/public/about/home.js
+++ app/public/about/home.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './home.css'
+import button from '../../components/button/button';
 
 function home(){
     return(
         <div>
             <h1>Home!!!</h1>
+            <p>Intro to React</p>
         </div>
+    
     )
 };
---
+++ app/src/pages/aboutyou/aboutyou.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from '../../components/button/button';
+function aboutyou (){
+    return (
+        <div>
+            aaaa
+            <Button/>
+        </div>
+    )
+}
+export default aboutyou;
